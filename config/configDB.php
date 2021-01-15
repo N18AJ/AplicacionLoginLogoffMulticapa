7a6d6d4928fd3ba7f23034e72335a0c75f249804
N18AJ <58330109+N18AJ@users.noreply.github.com>
2021-01-15 08:59:41 +0100

    Update configDB.php

--- config/configDB.php
+++ config/configDB.php
@@ -8,16 +8,16 @@
 
 
     //CONFIGURACIÓN ENTORNO DE DESARROLLO
-    
+    /*
     define('DNS', 'mysql:host=192.168.20.19;dbname=DAW212DBProyectoTema5');//Dirección IP y nombre de la base de datos
     define('USER', 'usuarioDAW212DBProyectoTema5');//Nombre de usuario de la base de datos
     define('PASSWORD', 'paso');//Contraseña del usuario de la base de datos
-   
+   */
 
     //CONFIGURACIÓN 1&1
-   /*
-    define('DNS', 'mysql:host=db5000278682.hosting-data.io;dbname=dbs272026');//Dirección IP y nombre de la base de datos
-    define('USER', 'dbu287777');//Nombre de usuario de la base de datos
+   
+    define('DNS', 'mysql:host=db5000278679.hosting-data.io;dbname=dbs272023');//Dirección IP y nombre de la base de datos
+    define('USER', 'dbu287774');//Nombre de usuario de la base de datos
     define('PASSWORD', 'Covid1234$');//Contraseña del usuario de la base de datos
-    */
+    
 ?>
